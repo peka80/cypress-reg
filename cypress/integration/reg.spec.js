a8bb7017c38d91bf8e97d75469199a2ad41843f8
Peka80 <petar.dragovic@gmail.com>
2021-12-14 10:53:00 +0100

    register with valid data

--- cypress/integration/reg.spec.js
+++ cypress/integration/reg.spec.js
@@ -1,10 +1,18 @@
 describe('registration test', () => {
 
-  it('login without email address', () => {
-    cy.visit('https://gallery-app.vivifyideas.com/');
+  xit('register with valid data', () => {
+    cy.visit('/');
     cy.url().should('eq', 'https://gallery-app.vivifyideas.com/');
 
     cy.get('a[href="/register"]').click();
     cy.url().should('eq', 'https://gallery-app.vivifyideas.com/register');
+
+    cy.get('#first-name').type('Petar');
+    cy.get('#last-name').type('Dragovic');
+    cy.get('#email').type('peka@gmail.com');
+    cy.get('#password').type('peka2808');
+    cy.get('#password-confirmation').type('peka2808');
+    cy.get('[type="checkbox"]').click();
+    cy.get('[type="submit"]').click();
   });
 });
